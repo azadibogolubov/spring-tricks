798d3c9fc5e30a2b96bdbd16669f679645ec58e3
Azadi Bogolubov <azadibogolubov@gmail.com>
2020-02-15 17:34:01 -0800

    Lecture 2

---
+++ src/main/java/com/example/demo/annotations/JSONGetMapping.java
@@ -0,0 +1,20 @@
+package com.example.demo.annotations;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+public @interface JSONGetMapping {
+    @AliasFor(annotation = RequestMapping.class, attribute = "produces")
+    String[] produces() default {};
+    @AliasFor(annotation = RequestMapping.class, attribute = "value")
+    String[] value() default {};
+}
